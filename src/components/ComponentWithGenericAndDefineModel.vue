3e939bf7f48c08f7ec22fd346837e3ebffeafa3c
Robbe <robbe.vaes@appwise.be>
2023-05-17 13:24:00 +0200

    Reproduction

---
+++ src/components/ComponentWithGenericAndDefineModel.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts" generic="T = string">
+defineProps<{ randomProp: string }>();
+const model = defineModel<T>();
+</script>
+
+<template>
+  <div>
+    <div>
+      {{ randomProp }}
+    </div>
+    <input style="background-color: red" v-model="model" />
+
+  </div>
+</template>
+
